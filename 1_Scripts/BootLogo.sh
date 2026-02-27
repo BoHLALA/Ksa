55f479f0b4837c0926daa4c05db4eaaa98037e5d
BO-hlala <bo-hlala@hotmail.com>
2026-02-27 05:59:16 +0300

    Update BootLogo.sh

--- 1_Scripts/BootLogo.sh
+++ 1_Scripts/BootLogo.sh
@@ -2,43 +2,69 @@
 
 TOKEN="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"
 
-FILE="BootLogo.tar.gz"
-API_URL="https://api.github.com/repos/BoHLALA/Ksa/contents/3_Plugins/$FILE?ref=main"
-TMP="/var/volatile/tmp/$FILE"
+#########################################################
+description= Developed By Bo.HLALA .. ^_^  ready to upgrade... Lets Fun !!!
+########################################################
 
-echo "============================================================="
-echo "Downloading $FILE using GitHub API ..."
-echo "============================================================="
+MY_FILE="BootLogo.tar.gz"
+MY_TMP_FILE="/var/volatile/tmp/"$MY_FILE
 
-wget \
-  --header="Authorization: token $TOKEN" \
-  --header="Accept: application/octet-stream" \
-  -O "$TMP" \
-  "$API_URL"
+#########################################################
 
-if [ -f "$TMP" ]; then
-    echo ""
-    echo "Extracting ..."
-    echo "============================================================="
+MY_SEP='============================================================='
+echo $MY_SEP
+echo 'Downloading '$MY_FILE' ...'
+echo $MY_SEP
+echo ''
+#API_URL="https://api.github.com/repos/BoHLALA/Ksa/contents/3_Plugins/$FILE?ref=main"
+wget -qO- https://raw.githubusercontent.com/BoHLALA/Ksa/main/1_Scripts/BootLogo.sh | sh
+#wget -O /var/volatile/tmp/BootLogo.tar.gz --no-check-certificate "https://api.github.com/repos/BoHLALA/Ksa/contents/3_Plugins/$FILE?ref=main"
 
-    tar -xzvf "$TMP" -C /
-    RESULT=$?
+rm -rf "/usr/share/enigma2/BoHLALA_FHD/k.s.a/Bootlogo"
 
-    rm -f "$TMP"
+if [ -f $MY_TMP_FILE ]; then
 
-    if [ $RESULT -eq 0 ]; then
-        echo "#############################################################"
-        echo "#     BootLogo INSTALLED SUCCESSFULLY - By BoHLALA          #"
-        echo "#############################################################"
+	echo ''
+	echo $MY_SEP
+	echo 'Extracting ...'
+	echo $MY_SEP
+	echo ''
+	tar -xzvf $MY_TMP_FILE -C /
+	MY_RESULT=$?
 
-        if which systemctl > /dev/null 2>&1; then
-            sleep 2; systemctl restart enigma2
-        else
-            init 4; sleep 4; init 3
-        fi
-    else
-        echo ">>>> INSTALLATION FAILED (Extraction Error) <<<<"
-    fi
+	rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+	echo ''
+	if [ $MY_RESULT -eq 0 ]; then
+        echo "###################################################################"
+        echo "#         skin BoHLALA_FHD v_2.2 INSTALLED SUCCESSFULLY           #"
+        echo "#                     Developed By Bo.HLALA                       #"
+        echo "#            https://www.tunisia-sat.com/forums/forums            #"
+        echo "#                https://www.linuxsat-support.com/                #"
+        echo "###################################################################"
+        echo "#        Sucessfully Download skin ... Lets Fun !!!  .. ^_^       #"
+        echo "###################################################################"
+		if which systemctl > /dev/null 2>&1; then
+			sleep 2; systemctl restart enigma2
+		else
+			init 4
+			sleep 4 > /dev/null 2>&1
+			init 3
+		fi
+	else
+		echo "   >>>>   INSTALLATION FAILED !   <<<<"
+	fi
+	 echo '**************************************************'
+	 echo '**                   FINISHED                   **'
+	 echo '**************************************************'
+	 echo ''
+	 exit 0
 else
-    echo ">>>> DOWNLOAD FAILED (File Not Found or Corrupted) <<<<"
+	 echo ''
+	 echo "Download failed !"
+	 exit 1
 fi
+# ----------------------------------------------------------------------------------------------------------
+fi
+
+
